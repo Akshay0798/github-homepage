3d7a2313e0ea94a4c0d861ff7fbfc180a31392f0
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com.>
2024-08-26 12:12:16 -0700

    hero section commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/HeroSection/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <div className="relative z-50">
         <div className="absolute">
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
       <div>
         <div className="overflow-hidden">
-          <div className="relative h-screen">
+          <div className="relative">
             <img
               className="absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] image"
               width="4377"
               src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
               alt=""
             />
+          </div>
+          <div className="hero-section px-3">
+            <Hero />
           </div>
         </div>
       </div>
